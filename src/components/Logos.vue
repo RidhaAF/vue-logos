c4d0ff08c9c66e4ff97ab559d7fb44435ab0d469
Ridha Ahmad Firdaus <41348944+RidhaAF@users.noreply.github.com>
2022-01-31 14:02:06 +0700

    Add Logos component

---
+++ src/components/Logos.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="logos">
+    <h2>Find Best Logo</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Logos",
+};
+</script>
+
+<style scoped>
+h2 {
+  color: #41b883;
+}
+</style>
