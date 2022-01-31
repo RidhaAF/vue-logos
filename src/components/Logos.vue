f3271e88f8aa0a25181e96d5e18c8aea45a7e1d4
Ridha Ahmad Firdaus <41348944+RidhaAF@users.noreply.github.com>
2022-01-31 14:21:38 +0700

    Update layout

--- src/components/Logos.vue
+++ src/components/Logos.vue
@@ -10,6 +10,18 @@
     <div class="item-logos">
       <img src="../assets/line.png" alt="Line" class="logo" />
     </div>
+    <div class="item-logos">
+      <img src="../assets/facebook.png" alt="Facebook" class="logo" />
+    </div>
+    <div class="item-logos">
+      <img src="../assets/instagram.png" alt="Instagram" class="logo" />
+    </div>
+    <div class="item-logos">
+      <img src="../assets/line.png" alt="Line" class="logo" />
+    </div>
+    <div class="item-logos">
+      <img src="../assets/facebook.png" alt="Facebook" class="logo" />
+    </div>
   </div>
 </template>
 
@@ -23,20 +35,22 @@ export default {
 .title-header {
   color: #41b883;
   margin-bottom: 80px;
+  text-align: left;
 }
 
 .logos-wrapper {
-  width: 70%;
+  width: 65%;
   overflow: hidden;
   margin: auto;
 }
 
 .item-logos {
   float: left;
-  margin-right: 20px;
-  margin-bottom: 20px;
+  margin-right: 50px;
+  margin-bottom: 50px;
   width: 150px;
   height: 150px;
+  text-align: center;
 }
 
 .item-logos .logo {
