cee9f6cfc2c68843d27e15a3e0f336abbc9f6ab6
Ridha Ahmad Firdaus <41348944+RidhaAF@users.noreply.github.com>
2022-01-31 14:05:24 +0700

    Update Logos component

--- src/components/Logos.vue
+++ src/components/Logos.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="logos">
+  <div class="logos-wrapper">
     <h2>Find Best Logo</h2>
+    <div class="item-logos">
+      <img src="" alt="" />
+    </div>
   </div>
 </template>
 
@@ -13,5 +16,20 @@ export default {
 <style scoped>
 h2 {
   color: #41b883;
+}
+
+.logos-wrapper {
+  width: 70%;
+  overflow: hidden;
+  margin: auto;
+}
+
+.item-logos {
+  float: left;
+  margin-right: 20px;
+  margin-bottom: 20px;
+  width: 150px;
+  height: 150px;
+  background: red;
 }
 </style>
